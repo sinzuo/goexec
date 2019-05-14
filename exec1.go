4444c2456b253629bf201800a094f719ec34fc63
root <1586706912@qq.com>
2019-05-14 22:58:16 +0800

    新增

---
+++ exec1.go
@@ -0,0 +1,33 @@
+// server
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+func handle(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("jiangyibo"))
+
+}
+
+//var s1 = http.Handle("bobo", handle)
+
+func main() {
+	c1 := exec.Command("echo", "jiang")
+	_, err := c1.StdinPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	var out bytes.Buffer
+	c1.Stdout = &out
+	c1.Run()
+	fmt.Println(out.String())
+	http.HandleFunc("/jiang", handle)
+	http.ListenAndServe(":9999", nil)
+	fmt.Println("Hello World!")
+}
